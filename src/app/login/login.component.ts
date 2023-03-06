809b55bdc184f01d3f70bc9c7c763fb7113a556c
unknown <hibaki1998@gmail.com>
2023-03-06 12:13:19 +0530

    Second Commit : Event Binding With Template Rendering Variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,32 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
- data="Your Perfect Banking Partner" //Example For String Interpolation
- data1="Enter Your Account No :" //Example For Property Binding
+  data = "Your Perfect Banking Partner" //Example For String Interpolation
+  data1 = "Enter Your Account No :" //Example For Property Binding
 
-accno:any
-pass:any
-// or accno="" Declaring variable to store value from $Event Binding Method
+  // accno: any
+  // pass: any
+  // or accno="" Declaring variable to store value from $Event Binding Method
 
-  userDetails:any={
-    1000:{username:"Rhysand",accno:1001,password:"abc@123",balance:0},
-    1002:{username:"Amren",accno:1002,password:"abc@123",balance:0},
-    1003:{username:"Cassian",accno:1003,password:"abc@123",balance:0},
-    1004:{username:"Azriel",accno:1004,password:"abc@123",balance:0},
-    1005:{username:"Lyssandra",accno:1005,password:"abc@123",balance:0},
-    1006:{username:"Yrene",accno:1006,password:"abc@123",balance:0},
+  userDetails: any = {
+    1000: { username: "Rhysand", accno: 1001, password: "abc@123", balance: 0 },
+    1002: { username: "Amren", accno: 1002, password: "abc@123", balance: 0 },
+    1003: { username: "Cassian", accno: 1003, password: "abc@123", balance: 0 },
+    1004: { username: "Azriel", accno: 1004, password: "abc@123", balance: 0 },
+    1005: { username: "Lyssandra", accno: 1005, password: "abc@123", balance: 0 },
+    1006: { username: "Yrene", accno: 1006, password: "abc@123", balance: 0 },
   }
 
-  constructor(){ }
+  constructor() { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
 
   }
 
   //methods
-  login(){
-    var accnum=this.accno
-    var pass=this.pass
+
+
+  // login(){
+  //   var accnum=this.accno
+  //   var pass=this.pass
+  //   if(accnum in this.userDetails){
+  //     if(pass==this.userDetails[accnum]["password"])
+  //     {
+  //       alert('Login Successfull')
+  //     }
+  //     else{
+  //       alert('Incorrect Password')
+  //     }
+  //   }
+
+  //   else{
+  //     alert('Not Registered Account !')
+  //   }
+  // }
+
+
+  //$Event Binding Method
+  // accnoChange(event:any){
+  //   console.log(event.target.value);
+  //   this.accno=event.target.value
+  // }
+
+  // passChange(event:any){
+  //   console.log(event.target.value);
+  //   this.pass=event.target.value
+  // }
+
+
+  login(accno:any,pass:any){
+console.log(accno.value,pass.value);
+
+
+
+    var accnum=accno.value
+    var pass=pass.value
     if(accnum in this.userDetails){
       if(pass==this.userDetails[accnum]["password"])
       {
@@ -47,16 +84,5 @@ pass:any
     }
   }
 
-  accnoChange(event:any){
-    console.log(event.target.value);
-    this.accno=event.target.value
-    
-  }
-
-  passChange(event:any){
-    console.log(event.target.value);
-    this.pass=event.target.value
-    
-  }
 
 }
